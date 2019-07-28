3b83eb1f61ef79a95455c2774ccc11ee10dd09a9
fummicc1 <fumiya.tennis1@gmail.com>
2019-07-29 00:24:10 +0900

    changeTime.time

--- main.go
+++ main.go
@@ -39,7 +39,9 @@ func NewRouter() *gin.Engine {
 		var diary Diary
 		err := c.BindJSON(&diary)
 		CheckErr(err)
-		diary.CreatedAt = time.Now().Format("2019/07/08")
+		currentTime := time.Now()
+		currentTime.In(time.Local)
+		diary.CreatedAt = currentTime.String()
 		diary.ID = xid.New().String()
 		db.Create(&diary)
 		c.JSON(200, diary)
